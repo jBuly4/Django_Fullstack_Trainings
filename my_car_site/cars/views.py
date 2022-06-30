eb85b4954e2a4ee64d484e62b4b9875948287d7d
jBuly4 <ibuly4@gmail.com>
2022-06-30 15:45:19 +0300

    forms, connection to models, add, delete and list objects from/to DB

--- my_car_site/cars/views.py
+++ my_car_site/cars/views.py
@@ -1,16 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from .models import Car
 
 # Create your views here.
 
 
 def list(request):
+    all_cars = Car.objects.all()
+    context = {
+        'all_cars': all_cars,
+    }
 
-    return render(request, 'cars/list.html')
+    return render(request, 'cars/list.html', context=context)
 
 
 def add(request):
-    return render(request, 'cars/add.html')
+    # print(request.POST) #  <QueryDict: {'csrfmiddlewaretoken': [
+    # 'Nw8n87Ihw6WcUZEGfcNdQHSgrpY1rJTdlInAPJkZpsR9Kx11EEkqGLPbn7VAzqNi'], 'brand': ['asas'], 'year': ['123']}>
+    if request.POST:
+        brand = request.POST['brand']
+        year = int(request.POST['year'])
+        Car.objects.create(brand=brand, year=year)  # after creating record redirect to list view
+
+        return redirect(reverse('cars:list'))
+    else:
+        return render(request, 'cars/add.html')
 
 
 def delete(request):
-    return render(request, 'cars/delete.html')
+    if request.POST:
+        pk = request.POST['pk']
+        try:
+            Car.objects.get(pk=pk).delete()
+            return redirect(reverse('cars:list'))
+        except:
+            print('pk not found!')
+            return redirect(reverse('cars:list'))
+    else:
+        return render(request, 'cars/delete.html')
