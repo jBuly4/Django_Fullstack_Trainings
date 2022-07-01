e7f10c246fed55820f2484f4653b3be1ee44bdf5
jBuly4 <ibuly4@gmail.com>
2022-07-01 22:45:57 +0300

    admin panel intro

--- my_car_site/cars/admin.py
+++ my_car_site/cars/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from cars.models import Car  # dunno why cars.models marked as mistake. maby because project is pure python?
 
 # Register your models here.
+
+# admin.site.register(Car)
+
+
+class CarAdmin(admin.ModelAdmin):
+    # customizing admin panel for Car model
+    # fields = ['year', 'brand']  # if you want to change order of field in admin panel
+    fieldsets = [
+        ('TIME INFORMATION', {'fields': ['year']}),  # will add labels with titles for your field or multiple fields
+        ('CAR INFORMATION', {'fields': ['brand']}),
+    ]
+    # additional information look in documentation of django
+
+admin.site.register(Car, CarAdmin)
