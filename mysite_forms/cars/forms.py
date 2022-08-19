908679f4a34d8e4a141e893ec3566eb5878844a4
jBuly4 <ibuly4@gmail.com>
2022-08-19 21:55:44 +0300

    forms: modelforms

--- mysite_forms/cars/forms.py
+++ mysite_forms/cars/forms.py
@@ -1,18 +1,18 @@
 from django import forms
+from .models import Review
+from django.forms import ModelForm
 
-
-class ReviewForm(forms.Form):
-    first_name = forms.CharField(label='First Name', max_length=100)  # explanation find in dojango docs
-    last_name = forms.CharField(label='Last Name', max_length=100)
-    email = forms.EmailField(label='email')
-    review = forms.CharField(label='Please write your review here',
-                             widget=forms.Textarea(
-                                     attrs={'class': 'myform',
-                                            'rows': '2',
-                                            'cols': '2'  # if we need to play with width of textarea
-                                            }
-                             ))
-
+# class ReviewForm(forms.Form):
+#     first_name = forms.CharField(label='First Name', max_length=100)  # explanation find in dojango docs
+#     last_name = forms.CharField(label='Last Name', max_length=100)
+#     email = forms.EmailField(label='email')
+#     review = forms.CharField(label='Please write your review here',
+#                              widget=forms.Textarea(
+#                                      attrs={'class': 'myform',
+#                                             'rows': '2',
+#                                             'cols': '2'  # if we need to play with width of textarea
+#                                             }
+#                              ))
 """
 after creating a form -> views import form -> change a view
 all fields have many parameters. one of them is widget. 
@@ -21,3 +21,9 @@ attrs -> dict with params -> styling class. that will allow to style exactly thi
 html for all forms
 but you can do it only inside widget parameter.
 """
+
+
+class ReviewForm(ModelForm):
+    class Meta:
+        model = Review
+        fields = ['first_name', 'last_name', 'stars']  # all fields we want to be in a form
