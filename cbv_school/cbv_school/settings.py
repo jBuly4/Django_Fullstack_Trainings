caef91fd0421ac11f3a36c9b33b301684da239b1
jBuly4 <ibuly4@gmail.com>
2022-08-20 21:14:43 +0300

    no migrations?

--- cbv_school/cbv_school/settings.py
+++ cbv_school/cbv_school/settings.py
@@ -47,6 +47,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # local
+    'classroom.apps.ClassroomConfig',
 ]
 
 ROOT_URLCONF = 'cbv_school.urls'
