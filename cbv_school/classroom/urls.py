a6329b7117053e26982689ab7e2fa89f7ad44cde
jBuly4 <ibuly4@gmail.com>
2022-08-20 21:48:21 +0300

    template view

--- cbv_school/classroom/urls.py
+++ cbv_school/classroom/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import home_view
-
+# from .views import home_view
+from .views import HomeView, ThankView
 app_name = 'classroom'
 
 
+# urlpatterns = [
+#     path('', home_view, name='home'),  # path expects a function
+# ]
+
 urlpatterns = [
-    path('', home_view, name='home'),  # path expects a function
+    path('', HomeView.as_view(), name='home'),
+    path('thank_you/', ThankView.as_view(), name='thank_you'),
 ]
--- cbv_school/classroom/views.py
+++ cbv_school/classroom/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
 
 
-def home_view(request):
-    return render(request, 'classroom/home.html')
+# def home_view(request):
+#     return render(request, 'classroom/home.html')
+
+
+class HomeView(TemplateView):
+    template_name = 'classroom/home.html'  # that's enough to render template
+
+
+class ThankView(TemplateView):
+    template_name = 'classroom/thank_you.html'
+
+# TemplateViews should be used in cases where all work will be done inside template
